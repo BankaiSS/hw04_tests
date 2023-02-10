553200b94c5932654d10a31a65251636d92f2802
Эльдар Байбанов <baibanove@mail.ru>
2023-02-11 02:24:02 +0300

    project done

---
+++ yatube/posts/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Группа'
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[:15]
+        
---
+++ yatube/posts/tests/test_forms.py
@@ -0,0 +1,78 @@
+from http import HTTPStatus
+ 
+from django.test import Client, TestCase
+from django.urls import reverse
+ 
+from ..forms import PostForm
+from ..models import Group, Post, User
+ 
+ 
+class PostCreateFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.author = User.objects.create_user(username='TestAuthor')
+        cls.auth_user = User.objects.create_user(username='TestAuthUser')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.author,
+            text='Тестовый текст поста',
+            group=cls.group,
+        )
+        cls.form = PostForm()
+ 
+    def setUp(self):
+        self.authorized_client = Client()
+        self.authorized_client_author = Client()
+        self.authorized_client.force_login(PostCreateFormTests.auth_user)
+        self.authorized_client_author.force_login(PostCreateFormTests.author)
+ 
+    def test_create_task(self):
+        """Валидная форма создает запись в Posts."""
+        post_count = Post.objects.count()
+        form_data = {
+            'text': 'Введенный в форму текст',
+            'group': self.group.id,
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        self.assertRedirects(
+            response,
+            reverse(
+                'posts:profile', kwargs={'username': self.auth_user.username}
+            )
+        )
+        self.assertEqual(Post.objects.count(), post_count + 1)
+        post = Post.objects.latest('id')
+        self.assertEqual(post.text, form_data['text'])
+        self.assertEqual(post.group_id, form_data['group'])
+ 
+    def test_author_edit_post(self):
+        """Валидная форма изменяет запись в Posts."""
+        form_data = {
+            'text': 'Отредактированный в форме текст',
+            'group': self.group.id,
+        }
+        self.authorized_client_author.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        post = Post.objects.get(id=self.group.id)
+        self.authorized_client_author.get(f'/posts/{post.id}/edit/')
+        response = self.authorized_client_author.post(
+            reverse('posts:post_edit', kwargs={'post_id': self.post.id}),
+            data=form_data,
+            follow=True
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        post = Post.objects.latest('id')
+        self.assertEqual(post.text, form_data['text'])
+        self.assertEqual(post.group_id, form_data['group'])
